c678e616f8f46256f91fdb7537d56ed3cdca2e88
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2017-11-11 00:13:40 -0500

    Declarations

--- mipsquestion.s
+++ mipsquestion.s
@@ -1,12 +1,16 @@
-
-	
 	.text
+	.global main
 main:
+	la $a0, prompt									#load the "prompt" string into $a0
+	li $v0, 4										#print prompt message
+	syscall
+
 	la $a0, hex_str
 	li $a1, 100
 	li $v0, 8		#read user input
 	syscall
 
+	add $t0, $zero, $zero
 
 	.data
 hex_str: .space 100
