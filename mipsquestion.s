da210c09745cc00ac971343d97ad9799cbdc15dd
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2017-11-15 20:53:06 -0500

    Code Made More Efficient

--- mipsquestion.s
+++ mipsquestion.s
@@ -1,3 +1,4 @@
+# HEXADECIMAL TO DECIMAL CONVERTER, by Sean Mills
 # $t0: sum
 # $t1: address
 # $t2: length
@@ -6,8 +7,8 @@
 # $t5: x, (16^n)
 # $t6: mult, the value of the hexadecimal digit in string (0, 1, 2,...,15)
 # $t7: product
-# $t8: quotient
-# $t9: remainder
+# $t8: quotient, and also the characters, '0'-'9' and 'A'-'F'
+# $t9: remainder, and also the characters, 'a'-'f'
 # decimal integer = (x * mult0) + (x * mult1) +...+ (x * multn)
 	
 	.text
@@ -47,31 +48,29 @@ end_space_check:
 	add $t1, $zero, $a0							# reset address pointer
 	
 length_loop:
-	lb $t3, ($t1)				# load a byte in hex string into $t3
-	beqz $t3, sub_length1		# if character is null, branch to sub_length
-	beq $t3, 10, sub_length2	# if character is '\n' branch to sub_length2
-	beq $t3, 32, skip_increment	# if the character is a space, do not increment length
-	add $t2, $t2, 1				# increment length
+	lb $t3, ($t1)					# load a byte in hex string into $t3
+	beqz $t3, sub_length1			# if character is null, branch to sub_length
+	beq $t3, 10, sub_length2		# if character is '\n' branch to sub_length2
+	beq $t3, 32, skip_increment		# if the character is a space, do not increment length
+	add $t2, $t2, 1					# increment length
 skip_increment:
-	addi $t1, $t1, 1			# move to the next character in the string,
-	b length_loop				# and repeat
-sub_length1:					# move length back 2 spaces, to represent the largest exponent
+	addi $t1, $t1, 1				# move to the next character in the string,
+	b length_loop					# and repeat
+sub_length1:						# move length back 2 spaces, to represent the largest exponent
 	sub $t2, $t2, 2
 	j reset_address
-sub_length2:					# move length back 1 space to represent the largest exponent
+sub_length2:						# move length back 1 space to represent the largest exponent
 	sub $t2, $t2, 1
 reset_address:
-	#bgt $t2, 7, error			# if length > 7 (largest possible exponent), display error message
-	#bltz $t2, error				# if length < 0, display error message
-	add $t1, $zero, $a0			# move pointer back to the first character
+	bgt $t2, 7, error				# if length > 7 (largest possible exponent), display error message
+	add $t1, $zero, $a0				# move pointer back to the first character
 
 # check_loop verifires each character in the hex string
 check_loop:
-	add $t4, $zero, $zero		# initialize power
-	add $t5, $zero, 1			# set x to 1
-	lb $t3, ($t1)				# load byte from address
-	j check_character0			# check what the character is
-
+	add $t4, $zero, $zero			# initialize power
+	add $t5, $zero, 1				# set x to 1
+	lb $t3, ($t1)					# load byte from address
+	j check_characters1				# check what the character is
 power_loop:
 	beq $t4, $t2, end_power_loop	# if power == length, then end loop
 	mulou $t5, $t5, 16				# x = x * 16
@@ -95,9 +94,8 @@ error:
 	li $v0, 4
 	syscall
 	j exit					
-
 print_decimal:
-	add $t9, $zero, 10		# store 10 in $t9
+	add $t9, $zero, 10				# store 10 in $t9
 	divu $t0, $t9					# split the value into 2 halves
 	mflo $t8						# store the first half in $t8
 	mfhi $t9						# store the second half in $t9
@@ -109,122 +107,41 @@ print_2nd_half:
 	move $a0, $t9					# display second half
 	li $v0, 1
 	syscall
-exit:
 	la $a0, new_line
 	li $v0, 4
 	syscall
+exit:
 	li $v0, 10						# exit
 	syscall
 
-check_character0:					
-	beq $t3, 32, end_check0			# if character is a space,
-	j check_character1
-end_check0:
+check_characters1:					
+	beq $t3, 32, end_check			# if character is a space,
+	j check_characters2
+end_check:
 	add $t1, $t1, 1					# ignore it, move to the next character, 
 	b check_loop					# and repeat check_loop
-check_character1:
-	beq $t3, 48, end_check1			# if character is '0',
-	j check_character2
-end_check1:			
-	add $t6, $zero, $zero			# mult = 0
-	b power_loop
-check_character2:
-	beq $t3, 49, end_check2			# if character is '1',
-	j check_character3
-end_check2:
-	add $t6, $zero, 1				# mult = 1
-	b power_loop
-check_character3:
-	beq $t3, 50, end_check3			# if character is '2',
-	j check_character4
-end_check3:
-	add $t6, $zero, 2				# mult = 2
-	b power_loop
-check_character4:
-	beq $t3, 51, end_check4			# if character is '3'
-	j check_character5
-end_check4:
-	add $t6, $zero, 3				# mult = 3
-	b power_loop
-check_character5:
-	beq $t3, 52, end_check5			# if character is '4'
-	j check_character6
-end_check5:
-	add $t6, $zero, 4				# mult = 4
-	b power_loop
-check_character6:
-	beq $t3, 53, end_check6			# if character is '5'
-	j check_character7
-end_check6:
-	add $t6, $zero, 5				# mult = 5
-	b power_loop
-check_character7:
-	beq $t3, 54, end_check7			# if character is '6'
-	j check_character8
-end_check7:
-	add $t6, $zero, 6				# mult = 6
-	b power_loop
-check_character8:
-	beq $t3, 55, end_check8			# if character is '7'
-	j check_character9
-end_check8:
-	add $t6, $zero, 7				# mult = 7
-	b power_loop
-check_character9:
-	beq $t3, 56, end_check9			# if character is '8'
-	j check_character10
-end_check9:
-	add $t6, $zero, 8				# mult = 8
-	b power_loop
-check_character10:
-	beq $t3, 57, end_check10		# if character is '9'
-	j check_character11
-end_check10:
-	add $t6, $zero, 9				# mult = 9
-	b power_loop
-check_character11:
-	beq $t3, 65, end_check11		# if character is 'A'
-	beq $t3, 97, end_check11		# or 'a'
-	j check_character12
-end_check11:
-	add $t6, $zero, 10				# mult = 10
-	b power_loop
-check_character12:
-	beq $t3, 66, end_check12		# if character is 'B'
-	beq $t3, 98, end_check12		# or 'b'
-	j check_character13
-end_check12:
-	add $t6, $zero, 11				# mult = 11
-	b power_loop
-check_character13:
-	beq $t3, 67, end_check13		# if character is 'C'
-	beq $t3, 99, end_check13		# or 'c'
-	j check_character14
-end_check13:
-	addi $t6, $zero, 12				# mult = 12
-	b power_loop
-check_character14:
-	beq $t3, 68, end_check14		# if character is 'D'
-	beq $t3, 100, end_check14		# or 'd'
-	j check_character15
-end_check14:
-	addi $t6, $zero, 13				# mult = 13
-	b power_loop
-check_character15:
-	beq $t3, 69, end_check15		# if character is 'E'
-	beq $t3, 101, end_check15		# or 'e'
-	j check_character16
-end_check15:
-	addi $t6, $zero, 14				# mult = 14
-	b power_loop
-check_character16:
-	beq $t3, 70, end_check16		# if character is 'F'
-	beq $t3, 102, end_check16		# or 'f', mult = 15
-	j error							# if none of the above, display error message
-end_check16:
-	addi $t6, $zero, 15				
-	b power_loop
+check_characters2:					
+	add $t8, $zero, 48				# set $t8 to the character, '0'
+	add $t6, $zero, $zero			# set mult to 0
+check_numbers:
+	beq $t8, 58, end_check_numbers	# when $t8 is outside the range of '0'-'9', check for letters
+	beq $t3, $t8, power_loop		# when character is found, go back to power_loop
+	add $t8, $t8, 1					# increment $t8
+	add $t6, $t6, 1					# increment mult
+	b check_numbers
+end_check_numbers:
+	add $t8, $zero, 65				# set $t8 to the character, 'A'
+	add $t9, $zero, 97				# set $t9 to the charatcer, 'a'
+check_letters:
+	beq $t8, 71, error				# if the character in the string is not a hexadecimal character, display error message
+	beq $t3, $t8, power_loop		# when character is found, go back to power_loop
+	beq $t3, $t9, power_loop		# when character is found, go back to power_loop
+	add $t8, $t8, 1					# increment $t8
+	add $t9, $t9, 1					# increment $t9
+	add $t6, $t6, 1					# increment mult
+	b check_letters					# and repeat
 
 	.data
 hex_str: .space 10
-new_line: .asciiz "\n\n"
+new_line: .asciiz "\n"
+error_msg: .asciiz "Invalid hexadecimal number."
