66b2d1bede7c9e5ffa6be09aba21f7e5a41f6ce3
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2017-11-15 19:34:01 -0500

    Checks for spaces in input

--- mipsquestion.s
+++ mipsquestion.s
@@ -3,45 +3,66 @@
 # $t2: length
 # $t3: character
 # $t4: power
-# $t5: x  (16^n)
-# $t6: mult, the value of the character in string (0, 1, 2,...,15)
+# $t5: x, (16^n)
+# $t6: mult, the value of the hexadecimal digit in string (0, 1, 2,...,15)
 # $t7: product
 # $t8: quotient
 # $t9: remainder
-# decimal = (x * mult0) + (x * mult1) +...+ (x * multn)
+# decimal integer = (x * mult0) + (x * mult1) +...+ (x * multn)
 	
 	.text
 main:
-	la $a0, prompt					# load the "prompt" string into $a0
-	li $v0, 4						# print prompt message
-	syscall
-
 	la $a0, hex_str
-	li $a1, 100
+	li $a1, 10
 	li $v0, 8						# read user input
 	syscall
 
-	add $t0, $zero, $zero			#initializ sum (which will eventually be the result)
+	add $t0, $zero, $zero			#initialize sum (which will eventually be the result)
 	add $t1, $zero, $a0				# store address of hex string in $t1
 	add $t2, $zero, $zero			# intialize length
 
+# check for spaces before, between, and after digits
+space_check:									
+	lb $t3, ($t1)
+	beq $t3, 10, error							# if there are no digits, display error message
+	beqz $t3, error
+	bne $t3, 32, check_for_space_after_char		# once a character is found that is not a space or \n,
+	add $t1, $t1, 1
+	b space_check
+check_for_space_after_char:						# check for spaces after that character
+	lb $t3, ($t1)
+	beq $t3, 10, end_space_check				# if there are no spaces after that, proceed with the program
+	beqz $t3, end_space_check
+	beq $t3, 32, check_for_char_after_space		# if there is a space after that, check if there are anymore digits
+	add $t1, $t1, 1
+	b check_for_space_after_char
+check_for_char_after_space:
+	lb $t3, ($t1)
+	beq $t3, 10, end_space_check				# if there are no more non-space characters, proceed with the program
+	beqz $t3, end_space_check
+	bne $t3, 32, error							# if there is a non-space character, display error message
+	add $t1, $t1, 1
+	b check_for_char_after_space
+end_space_check:								
+	add $t1, $zero, $a0							# reset address pointer
+	
 length_loop:
-	lb $t3, ($t1)				# load the byte in hex string into $t3
+	lb $t3, ($t1)				# load a byte in hex string into $t3
 	beqz $t3, sub_length1		# if character is null, branch to sub_length
 	beq $t3, 10, sub_length2	# if character is '\n' branch to sub_length2
-	beq $t3, 32, skip_increment
+	beq $t3, 32, skip_increment	# if the character is a space, do not increment length
 	add $t2, $t2, 1				# increment length
 skip_increment:
 	addi $t1, $t1, 1			# move to the next character in the string,
 	b length_loop				# and repeat
 sub_length1:					# move length back 2 spaces, to represent the largest exponent
 	sub $t2, $t2, 2
-	j test_length
+	j reset_address
 sub_length2:					# move length back 1 space to represent the largest exponent
 	sub $t2, $t2, 1
-test_length:
-	bgt $t2, 7, error			# if length > 7 (largest possible exponent), display error message
-	bltz $t2, error				# if length < 0, display error message
+reset_address:
+	#bgt $t2, 7, error			# if length > 7 (largest possible exponent), display error message
+	#bltz $t2, error				# if length < 0, display error message
 	add $t1, $zero, $a0			# move pointer back to the first character
 
 # check_loop verifires each character in the hex string
@@ -49,7 +70,7 @@ check_loop:
 	add $t4, $zero, $zero		# initialize power
 	add $t5, $zero, 1			# set x to 1
 	lb $t3, ($t1)				# load byte from address
-	j check_character1			# check what the character is
+	j check_character0			# check what the character is
 
 power_loop:
 	beq $t4, $t2, end_power_loop	# if power == length, then end loop
@@ -76,10 +97,7 @@ error:
 	j exit					
 
 print_decimal:
-	la $a0, dec_str					# display "Decimal Number:"
-	li $v0, 4
-	syscall
-	add $t9, $zero, 10000			# store 10000 in $t9
+	add $t9, $zero, 10		# store 10 in $t9
 	divu $t0, $t9					# split the value into 2 halves
 	mflo $t8						# store the first half in $t8
 	mfhi $t9						# store the second half in $t9
@@ -91,13 +109,19 @@ print_2nd_half:
 	move $a0, $t9					# display second half
 	li $v0, 1
 	syscall
+exit:
 	la $a0, new_line
 	li $v0, 4
 	syscall
-exit:
 	li $v0, 10						# exit
 	syscall
 
+check_character0:					
+	beq $t3, 32, end_check0			# if character is a space,
+	j check_character1
+end_check0:
+	add $t1, $t1, 1					# ignore it, move to the next character, 
+	b check_loop					# and repeat check_loop
 check_character1:
 	beq $t3, 48, end_check1			# if character is '0',
 	j check_character2
@@ -196,14 +220,11 @@ end_check15:
 check_character16:
 	beq $t3, 70, end_check16		# if character is 'F'
 	beq $t3, 102, end_check16		# or 'f', mult = 15
-	j error							# if none or the above, display error message
+	j error							# if none of the above, display error message
 end_check16:
 	addi $t6, $zero, 15				
 	b power_loop
 
 	.data
-hex_str: .space 9
-prompt: .asciiz "Enter Hexadecimal: "
-error_msg: .asciiz "Invalid Hexadcimal Number\n"
-dec_str: .asciiz "Decimal: "
+hex_str: .space 10
 new_line: .asciiz "\n\n"
